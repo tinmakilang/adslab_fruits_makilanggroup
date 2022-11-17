52cfd777ef22c37b20e27f55fe1d43cce533618c
tinmakilang <makilangchristine@gmail.com>
2022-11-17 01:31:56 -0800

    added by nakilang

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>HOME</title>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+	 <nav class="navbar navbar-light navbar-expand-lg bg-dark py-lg-4" id="mainNav">
+        <div class="container"><a class="navbar-brand text-uppercase text-expanded" href="#">Group 1</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="nav navbar-nav mx-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="/">Home</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="login.php">Log In</a></li>
+         <!--            <li class="nav-item" role="presentation"><a class="nav-link" href="products.html">Login</a></li> -->
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="registration.php">Register</a></li>
+                </ul>
+        </div>
+        </div>
+    </nav>
+    
+</body>
+</html>
